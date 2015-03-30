02b893e1ec8403bfd25ef16bbf965b78ff066768
kylelowtharp <klv9700@saintjoe.edu>
2015-03-30 13:46:03 -0400

    adding middile initial modify practicum#

--- reverseNames.js
+++ reverseNames.js
@@ -1,3 +1,8 @@
+//Kyle Lowtharp modify code practicm
+//the only thing I did was put the middle initial after the first name
+//I couldnt think of any way to cahnge the regex function for it to work
+
+
 print(
-  "Hopper, Grace\nMcCarthy, John\nRitchie, Dennis"
+  "Hopper, Grace N\nMcCarthy, John L\nRitchie, Dennis P"
     .replace(/([\w ]+), ([\w ]+)/g, "$2 $1"));
