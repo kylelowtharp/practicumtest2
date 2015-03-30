8fb84735e06d8aab7ed551769fa62058b8e5829a
kylelowtharp <klv9700@saintjoe.edu>
2015-03-30 13:45:10 -0400

    counter modify practicumf#	modified:   counter.js

--- counter.js
+++ counter.js
@@ -1,10 +1,17 @@
-var stock = "1 lemon, 2 cabbages, and 101 eggs";
+var stock = "1 lemon, 2 cabbages, and 105 eggs";
 function minusOne(match, amount, unit) {
   amount = Number(amount) - 1;
   if (amount == 1) // only one left, remove the 's'
     unit = unit.slice(0, unit.length - 1);
   else if (amount == 0)
+
+
     amount = "no";
+
+//change code so if over 100 it says zillions
+
+	else if (amount > 100)
+	amount = "zillions of";
   return amount + " " + unit;
 }
 print(stock.replace(/(\d+) (\w+)/g, minusOne));
